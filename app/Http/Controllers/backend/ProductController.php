b1153e8e14decea9e990d8065082954a501b1161
db-hasan <infoalihasanbd@gmail.com>
2024-01-18 12:27:28 +0600

    updt

--- app/Http/Controllers/backend/ProductController.php
+++ app/Http/Controllers/backend/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Product;
 use Session;
@@ -211,6 +213,9 @@ class ProductController extends Controller
             } 
         };
     }
+
+
+
 
     // public function update(Request $request, int $id)
     // {
